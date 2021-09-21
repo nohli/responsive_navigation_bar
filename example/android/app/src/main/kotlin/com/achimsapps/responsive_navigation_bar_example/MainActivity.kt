4a0e064ff1d0672bd0aa48a3e7a93a7ee158ecdf
nohli <43643339+nohli@users.noreply.github.com>
2021-09-21 20:39:24 +0200

    Remove unused files and folders

--- example/android/app/src/main/kotlin/com/achimsapps/responsive_navigation_bar_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.achimsapps.responsive_navigation_bar_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- macos/Classes/ResponsiveNavigationBarPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <FlutterMacOS/FlutterMacOS.h>
-
-@interface ResponsiveNavigationBarPlugin : NSObject<FlutterPlugin>
-@end
